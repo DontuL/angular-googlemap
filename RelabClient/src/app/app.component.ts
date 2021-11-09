78dc1b7d22d7030fde093e655b39b24d638c2fbc
DontuL <dontu.lilian@iisgalvanimi.edu.it>
2021-11-09 12:56:34 +0000

    meta

--- RelabClient/src/app/app.component.ts
+++ RelabClient/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { AfterViewInit } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
-import { GoogleMap } from '@angular/google-maps'
+import { GoogleMap, MapMarker } from '@angular/google-maps'
 import { Observable } from 'rxjs';
+import { Ci_vettore } from './models/Ci_vettore.models';
 import {  GeoFeatureCollection } from './models/geojson.model';
 
 @Component({
@@ -19,8 +20,11 @@ export class AppComponent implements AfterViewInit {
   // Centriamo la mappa
   center: google.maps.LatLngLiteral = { lat: 45.506738, lng: 9.190766 };
   zoom = 8;
+  obsCiVett : Observable<Ci_vettore[]>; //Crea un observable per ricevere i vettori energetici
+markerList : google.maps.MarkerOptions[];
 
   constructor(public http: HttpClient) {
+    
     //Facciamo iniettare il modulo HttpClient dal framework Angular (ricordati di importare la libreria)
   }
 
@@ -32,8 +36,37 @@ export class AppComponent implements AfterViewInit {
 
  //Una volta che la pagina web è caricata, viene lanciato il metodo ngOnInit scarico i    dati 
   //dal server
-  ngOnInit() {
-    this.obsGeoData = this.http.get<GeoFeatureCollection>("TUO_URL/ci_vettore/50");
-    this.obsGeoData.subscribe(this.prepareData);
+  findImage(label: string) : google.maps.Icon {
+    if (label.includes("Gas")) {
+      return { url: './assets/img/gas.ico', scaledSize: new google.maps.Size(32,32) };
+    }
+    if (label.includes("elettrica")) {
+      return { url: './assets/img/electricity.ico', scaledSize: new google.maps.Size(32,32) };
+    }
+    return {url: './assets/img/undefined.ico' , scaledSize: new google.maps.Size(32,32)}
   }
+  prepareCiVettData = (data: Ci_vettore[]) =>
+  {
+    console.log(data); //Verifica di ricevere i vettori energetici
+    this.markerList = []; //NB: markers va dichiarata tra le proprietà markers : Marker[]
+    for (const iterator of data) { //Per ogni oggetto del vettore creo un Marker
+      let m : google.maps.MarkerOptions = 
+      {
+       position : new google.maps.LatLng (iterator.WGS84_X, iterator.WGS84_Y),
+       label : iterator.CI_VETTORE 
+      }
+      //Marker(iterator.WGS84_X,iterator.WGS84_Y,iterator.CI_VETTORE);
+      this.markerList.push(m);
+    }
+  }
+  
+  ngAfterViewInit() {
+    console.log("ciao");
+    this.obsGeoData = this.http.get<GeoFeatureCollection>("https://5000-blush-cat-tl1jbojr.ws-eu18.gitpod.io/ci_vettore/50");
+    this.obsGeoData.subscribe(this.prepareData);
+    this.obsCiVett = this.http.get<Ci_vettore[]>("https://5000-blush-cat-tl1jbojr.ws-eu18.gitpod.io/ci_vettore/140");
+this.obsCiVett.subscribe(this.prepareCiVettData);
+    
+  }
+  
 }
